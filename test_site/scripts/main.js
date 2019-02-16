742901c2227a31c45618cf7c4274ddf4a2fa68d8
BamLin <765143362@qq.com>
2019-02-16 14:33:56 +0800

    Add files via upload

---
+++ test_site/scripts/main.js
@@ -0,0 +1,37 @@
+// let myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Hello world!';
+// 注释
+/**注释 */
+/*注释 */
+
+let myImage = document.querySelector('img');
+//  onclick  点击 切换图片
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+      myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
+
+function setHeading(name) {
+  let myHeading = document.querySelector('h1');
+  myHeading.textContent = 'Mozilla 酷毙了，' + name + '！';
+}
+
+function setUserName() {
+  let myName = prompt('请输入你的名字');
+  localStorage.setItem('name', myName);
+  setHeading(myName);
+} 
+
+let storedName = localStorage.getItem('name');
+if(!storedName) {
+   setUserName();
+} else {
+   setHeading(storedName);
+}
+
+let myButton = document.querySelector('button'); 
+myButton.onclick = setUserName;
